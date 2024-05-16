0d77d5434c48d8d2cc049e8caf48c3231e96d950
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-05-16 11:05:34 +0200

    added color

--- simulator/visualizer.py
+++ simulator/visualizer.py
@@ -22,25 +22,41 @@ class Visualizer:
                 else:
                     perspective_coef = center[1] / self.world.height
                     if plant_types[i] == 0:
-                        cv.circle(self.img, center, int(20 * perspective_coef), 255, -1)
+                        cv.circle(self.img, center, int(20 * perspective_coef), (255, 255, 255), -1)
                     elif plant_types[i] == 1:
-                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_CROSS,
+                        cv.drawMarker(self.img, center, (255, 255, 255), markerType=cv.MARKER_CROSS,
                                       markerSize=int(50 * perspective_coef), thickness=5)
                     elif plant_types[i] == 2:
-                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_TILTED_CROSS,
+                        cv.drawMarker(self.img, center, (255, 255, 255), markerType=cv.MARKER_TILTED_CROSS,
                                       markerSize=int(50 * perspective_coef), thickness=5)
                     else:
-                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_STAR,
+                        cv.drawMarker(self.img, center, (255, 255, 255), markerType=cv.MARKER_STAR,
                                       markerSize=int(50 * perspective_coef), thickness=5)
 
     def draw_particles(self, particle_filter):
-        # for i in range(particle_filter.n_particles):
-        # center = (self.world.width/2, particle_filter.particles[i][1][0])
-        pass
+        for i in range(particle_filter.n_particles):
+            # Coordinates of the particle
+            center = np.asarray([int(self.world.width/2), int(particle_filter.particles[i][1][0])])
+
+            perspective_coef = center[1] / self.world.height
+
+            # Color of the particle in fonction of the its weight
+            if particle_filter.particles[i][0] > 0.70:
+                color = (0, 0, 255)
+                thickness = 5
+            elif particle_filter.particles[i][0] > 0.40:
+                color = (0, 255, 0)
+                thickness = 4
+            else:
+                color = (255, 0, 0)
+                thickness = 2
+
+            cv.drawMarker(self.img, center, color, markerType=cv.MARKER_DIAMOND,
+                          markerSize=int((7*thickness) * perspective_coef), thickness=thickness)
 
     def draw(self, plants, particle_filter):
         # Empty image
-        self.img = np.zeros((self.world.height, self.world.width), np.uint8)
+        self.img = np.zeros((self.world.height, self.world.width, 3), np.uint8)
 
         # Draw plants and particles
         self.draw_plants(plants)
