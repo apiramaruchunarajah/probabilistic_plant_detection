0b51b01cad31b165ed6d626fb57d97ec5a4a2054
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-05-17 10:41:18 +0200

    added comments

--- core/particle_filters/particle_filter_base.py
+++ core/particle_filters/particle_filter_base.py
@@ -2,26 +2,13 @@ from abc import abstractmethod
 import copy
 import numpy as np
 
+# Modified code from :
+# Jos Elfring, Elena Torta, and René van de Molengraft.
+# Particle filters: A hands-on tutorial.
+# Sensors, 21(2), 2021.
 
 class ParticleFilter:
-    """
-    Notes:
-        * State is (x, y, heading), where x and y are in meters and heading in radians
-        * State space assumed limited size in each dimension, world is cyclic (hence leaving at x_max means entering at
-        x_min)
-        * Abstract class
-    """
-
     def __init__(self, number_of_particles, limits, process_noise, measurement_noise):
-        """
-        Initialize the abstract particle filter.
-
-        :param number_of_particles: Number of particles
-        :param limits: List with maximum and minimum values for x and y dimension: [xmin, xmax, ymin, ymax]
-        :param process_noise: Process noise parameters (standard deviations): [std_forward, std_angular]
-        :param measurement_noise: Measurement noise parameters (standard deviations): [std_range, std_angle]
-        """
-
         if number_of_particles < 1:
             print("Warning: initializing particle filter with number of particles < 1: {}".format(number_of_particles))
 
@@ -30,6 +17,7 @@ class ParticleFilter:
         self.particles = []
 
         # State related settings
+        # For the moment we are only tracking the position parameter
         self.state_dimension = 1
         #self.inter_plant_min = limits[0]
         #self.inter_plant_max = limits[1]
@@ -51,11 +39,6 @@ class ParticleFilter:
         self.measurement_noise = measurement_noise
 
     def initialize_particles_uniform(self):
-        """
-        Initialize the particles uniformly over the world assuming a 3D state (x, y, heading). No arguments are required
-        and function always succeeds hence no return value.
-        """
-
         # Initialize particles with uniform weight distribution
         self.particles = []
         weight = 1.0 / self.n_particles
@@ -69,14 +52,7 @@ class ParticleFilter:
         self.print_particles()
 
     def validate_state(self, state):
-        """
-        Validate the state. State values outide allowed ranges will be corrected for assuming a 'cyclic world'.
-
-        :param state: Input particle state.
-        :return: Validated particle state.
-        """
-
-        # Make sure state does not exceed allowed limits (cyclic world)
+        # Make sure state does not exceed allowed limits
         # TODO: find a better way of validating states
 
         while state[0] < self.position_min:
@@ -87,22 +63,9 @@ class ParticleFilter:
 
         return state
 
-    def set_particles(self, particles):
-        """
-        Initialize the particle filter using the given set of particles.
-
-        :param particles: Initial particle set: [[weight_1, [x1, y1, theta1]], ..., [weight_n, [xn, yn, thetan]]]
-        """
-
-        # Assumption: particle have correct format, set particles
-        self.particles = copy.deepcopy(particles)
-        self.n_particles = len(self.particles)
-
     def get_average_state(self):
         """
         Compute average state according to all weighted particles
-
-        :return: Average x-position, y-position and orientation
         """
 
         # Compute sum of all weights
@@ -158,16 +121,6 @@ class ParticleFilter:
 
     # p(xk | xk-1)
     def propagate_sample(self, sample, motion_move_distance):
-        """
-        Propagate an individual sample with a simple motion model that assumes the robot rotates angular_motion rad and
-        then moves forward_motion meters in the direction of its heading. Return the propagated sample (leave input
-        unchanged).
-
-        :param motion_move_distance: Plants' move distance
-        :param sample: Sample (unweighted particle) that must be propagated
-        :return: propagated sample
-        """
-
         propagated_sample = copy.deepcopy(sample)
 
         # Compute forward motion by combining deterministic forward motion with additive zero mean Gaussian noise
@@ -183,13 +136,10 @@ class ParticleFilter:
         # Make sure we stay within cyclic world
         return self.validate_state(propagated_sample)
 
+    # p(zk / xk)
     def compute_likelihood(self, sample, measurement):
         """
         Compute likelihood p(z|sample) for a specific measurement given sample state and landmarks.
-
-        :param sample: Sample (unweighted particle) that must be propagated
-        :param measurement: For the moment is not a list #TODO : make it a list
-        :return Likelihood
         """
 
         # Initialize measurement likelihood
@@ -199,7 +149,8 @@ class ParticleFilter:
         expected_position = sample[0]
 
         # Map difference true and expected distance measurement to probability
-        # Normal distribution #TODO : think about Bernouilli distribution
+        # Normal distribution
+        #TODO : think about Bernouilli distribution
         pr_z_given_position = \
             np.exp(-(expected_position - measurement) * (expected_position - measurement) /
                    (2 * self.measurement_noise[0] * self.measurement_noise[0]))
@@ -210,13 +161,12 @@ class ParticleFilter:
         # We will probably need to use multiplication when using the 7 parameters
         likelihood_sample *= pr_z_given_position
 
-        # Return importance weight based on all landmarks
         return likelihood_sample
 
     @abstractmethod
     def update(self, plants_motion_move_distance, measurement):
         """
-        Process a measurement given the measured robot displacement. Abstract method that must be implemented in derived
+        Process a measurement given the measured plants displacement. Abstract method that must be implemented in derived
         class.
 
         """
