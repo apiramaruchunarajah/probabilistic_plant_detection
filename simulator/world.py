8a124ac0698ae5519c6607101e4df86764acfd7d
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-05-24 13:25:32 +0200

    added coordinates checking method

--- simulator/world.py
+++ simulator/world.py
@@ -3,3 +3,9 @@ class World:
         self.width = width
         self.height = height
         self.speed = speed
+
+    def are_coordinates_valid(self, x, y):
+        """
+        Returns a boolean indicating whether (x, y) is inside the world/image or not (valid position or not).
+        """
+        return not (x < 0 or x > self.width or y < 0 or y > self.height)
