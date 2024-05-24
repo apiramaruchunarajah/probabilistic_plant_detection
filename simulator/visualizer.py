c319718d88550364eb85040c0739c6fa273f7f7a
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-05-24 14:19:27 +0200

    moved get methods to the particle class

--- simulator/visualizer.py
+++ simulator/visualizer.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2 as cv
 
-from .world import World
 from .particle import Particle
 
 
@@ -52,72 +51,34 @@ class Visualizer:
                 color = (0, 255, 0)
                 thickness = 2
 
+            # We consider now binary images
+            color = (255, 255, 255)
             cv.drawMarker(self.img, center, color, markerType=cv.MARKER_DIAMOND,
                           markerSize=int((7 * thickness) * perspective_coef), thickness=thickness)
 
     def draw_complete_particle(self, particle):
 
-        # Getting bottom plants coordinates and the number of right and left plants
-        bottom_plants, nb_left_plants, nb_right_plants = particle.get_bottom_plants()
+        # Getting the coordinates of every plant to draw
+        plants = particle.get_all_plants()
 
-        # Drawing parameters
-        color = (255, 255, 255)
-        markerType = cv.MARKER_STAR
+        # Drawing all the plants
+        for center in plants:
+            perspective_coef = center[1] / self.world.height
+            color = (255, 255, 255)
 
-        # Drawing the plants located at the bottom
-        perspective_coef = particle.position / self.world.height
-
-        for center in bottom_plants:
-            cv.drawMarker(self.img, center, color, markerType=markerType,
+            cv.drawMarker(self.img, center, color, markerType=cv.MARKER_TRIANGLE_UP,
                           markerSize=int(40 * perspective_coef), thickness=4)
-
-        # Getting the crossing point between the row where the particular plant is and the top of the image.
-        top_particular_crossing_point = particle.get_particular_plant_top_crossing_point()
-
-        # Getting the crossing point for each row : point of intersection between a row and the top of the image.
-        top_crossing_points = particle.get_all_top_crossing_points(top_particular_crossing_point[0],
-                                                                   top_particular_crossing_point[1], nb_left_plants,
-                                                                   nb_right_plants)
-
-        if len(bottom_plants) != len(top_crossing_points):
-            print("Error: number of bottom plants and number of top crossing points are not equal.")
-            return -1
-
-        # Getting the vanishing point
-        # We could take any two plants to compute the vanishing point, here plant 0 and 1
-        if len(bottom_plants) < 2:
-            print("Can not compute the vanishing point.")
-            return -1
-
-        vanishing_point = particle.get_vanishing_point(bottom_plants[0], top_crossing_points[0],
-                                                       bottom_plants[1], top_crossing_points[1])
-
-        print("Vanishing point1 : {}".format(vanishing_point))
-
-        # Drawing all the remaining plants row by row.
-        # For each row
-        for i in range(len(bottom_plants)):
-            bottom_plant = bottom_plants[i]
-
-            # Getting the coordinates of the plants located in the row
-            row_plants = particle.get_row_plants(bottom_plant, vanishing_point)
-
-            # Drawing all the plants located in the row
-            for center in row_plants:
-                perspective_coef = center[1] / self.world.height
-                cv.drawMarker(self.img, center, color, markerType=markerType,
-                              markerSize=int(40 * perspective_coef), thickness=4)
 
     def draw(self, plants, particles, n_particles):
         # Empty image
-        self.img = np.zeros((self.world.height, self.world.width, 3), np.uint8)
+        self.img = np.zeros((self.world.height, self.world.width), np.uint8)
 
         # Draw plants and particles
         self.draw_plants(plants)
         self.draw_particles(particles, n_particles)
 
         # Testing of the function draw_complete_particle
-        self.img = np.zeros((self.world.height, self.world.width, 3), np.uint8)
+        # self.img = np.zeros((self.world.height, self.world.width, 3), np.uint8)
 
         offset = 240
         position = self.world.height - 40
@@ -129,4 +90,4 @@ class Visualizer:
 
         particle = Particle(self.world, offset, position, ir, ip, convergence, skew)
 
-        self.draw_complete_particle(particle)
+        # self.draw_complete_particle(particle)
