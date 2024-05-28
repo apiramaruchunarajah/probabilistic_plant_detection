1e6271ef1f423aa04edb0a447ba13671e81b09fe
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-05-28 10:29:55 +0200

    propagate particles working

--- core/particle_filters/particle_filter_sir.py
+++ core/particle_filters/particle_filter_sir.py
@@ -15,7 +15,7 @@ class ParticleFilterSIR(ParticleFilter):
                  measurement_noise,
                  resampling_algorithm):
 
-        # Initialize particle filter base class
+        # Initialize particle0 filter base class
         ParticleFilter.__init__(self, number_of_particles, limits, process_noise, measurement_noise)
 
         # Set SIR specific properties
@@ -24,7 +24,7 @@ class ParticleFilterSIR(ParticleFilter):
 
     def needs_resampling(self):
         """
-        Method that determines whether not a core step is needed for the current particle filter state estimate.
+        Method that determines whether not a core step is needed for the current particle0 filter state estimate.
         The sampling importance core (SIR) scheme resamples every time step hence always return true.
 
         :return: Boolean indicating whether or not core is needed.
@@ -35,14 +35,15 @@ class ParticleFilterSIR(ParticleFilter):
         # Loop over all particles
         new_particles = []
         for par in self.particles:
-            # Propagate the particle state according to the current particle
+            # Propagate the particle0 state according to the current particle0
             propagated_state = self.propagate_sample(par[1], plants_motion_move_distance)
 
-            # Compute current particle's weight
-            weight = par[0] * self.compute_likelihood(propagated_state, measurement)
+            # Compute current particle0's weight
+            #weight = par[0] * self.compute_likelihood(propagated_state, measurement)
 
             # Store
-            new_particles.append([weight, propagated_state])
+            #new_particles.append([weight, propagated_state])
+            new_particles.append([par[0], propagated_state])
 
         # Update particles
         self.particles = self.normalize_weights(new_particles)
